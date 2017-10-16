4ba9c0f302fc439113f9d1eaa8777ee6021a3606
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-10-16 11:34:54 +0200

    init

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+// fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})
+
+//categories
+'GET_CATEGORIES'
+'GET_CATEGORY_POSTS'
+
+// all posts
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+
+// single post
+export const GET_POST = 'GET_POST';
+export const ADD_POST = 'ADD_POST';
+export const DELETE_POST = 'DELETE_POST';
+
+'GET_POST_DETAILS'
+'UPVOTE_POST'
+'DOWNVOTE_POST'
+
+
+// all comments
+'GET_POST_COMMENTS' ;
+
+// single comment
+'ADD_COMMENT'
+'EDIT_COMMENT'
+'UPVOTE_COMENT' 
+'DOWNVOTE_COMMENT'
+'DELETE_COMMENT'
+
+
+// comment details
+
+'GET_COMMENT_DETAILS'
+'EDIT_COMMENT_DETAILS'
