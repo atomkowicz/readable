440edcb7136e4cdd870a95c79778838104edfd48
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-10-21 12:46:14 +0200

    Add api file with getAllPosts

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001';
+
+let token = localStorage.token;
+if (!token)
+    token = localStorage.token = Math.random().toString(36).substring(-8);
+
+const headers = {
+    Accept: 'application/json',
+    Authorization: token
+}
+
+export const getAllPosts = () => {
+    fetch('${api}/posts', {
+            headers
+        })
+        .then(res => res.json)
+        .then(data => data.posts)
+}
