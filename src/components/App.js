5834d3767872e656e645c4bacc2769bc96967deb
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-10-21 15:12:08 +0200

    Add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,32 @@
 import React, { Component } from 'react';
 import '../assets/styles/App.css';
+import * as ReadableAPI from '../utils/api';
+import Header from './Header';
+import PostList from './PostList';
 
 class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount = () => {
+    ReadableAPI.getAllPosts().then((posts) => {
+      this.setState({ posts });
+    })
+  }
+
+
   render() {
+
     return (
       <div className="App">
-
-        <header className="App-header">
-          <h1 className="App-title">Readable - all posts</h1>
-        </header>
-        <div className="list-posts">
-          <ol className="post-list">
-            <li className="post-list-item">
-              <div className="post-voting-box" >
-                <button className="post-upvote">Upvote</button>
-                <div className="post-score">
-                  <p>Score: <span>19</span></p>
-                </div>
-                <button className="post-downvote">Downvote</button>
-              </div>
-              <div className="post-details">
-                <div className="post-info">
-                  <span className="post-author">Author: me</span>
-                </div>
-                <h3>Are posts listed correctly and have the desired functionality in a list view?</h3>
-                <p>Listed posts are displayed with title, author, number of comments, current score, and a voting mechanism to upvote or downvote the post. Posts should have buttons or links for editing or deleting that post.
-The voting mechanism works and correctly displays the new vote score after clicking.
-List posts link to the detail page for that post.
-All posts are listed at the root.
-All posts for a category are listed at /:category
-List pages (root or category) include a mechanism for sorting by date or by score (at a minimum) and the sort works properly.</p>
-                <div className="post-info">
-                  <a href="" className="post-comments">Comments: 34</a>&nbsp;&nbsp;&nbsp;
-                  <a href="" className="post-edit">Edit</a>&nbsp;&nbsp;&nbsp;
-                  <a href="" className="post-delete">Delete</a>
-                </div>
-              </div>
-            </li>
-            <li className="post-list-item">
-            <div className="post-voting-box" >
-              <button className="post-upvote">Upvote</button>
-              <div className="post-score">
-                <p>Score: <span>19</span></p>
-              </div>
-              <button className="post-downvote">Downvote</button>
-            </div>
-            <div className="post-details">
-              <div className="post-info">
-                <span className="post-author">Author: me</span>
-              </div>
-              <h3>Are posts listed correctly and have the desired functionality in a list view?</h3>
-              <p>Listed posts are displayed with title, author, number of comments, current score, and a voting mechanism to upvote or downvote the post. Posts should have buttons or links for editing or deleting that post.
-The voting mechanism works and correctly displays the new vote score after clicking.
-List posts link to the detail page for that post.
-All posts are listed at the root.
-All posts for a category are listed at /:category
-List pages (root or category) include a mechanism for sorting by date or by score (at a minimum) and the sort works properly.</p>
-              <div className="post-info">
-                <a href="" className="post-comments">Comments: 34</a>&nbsp;&nbsp;&nbsp;
-                <a href="" className="post-edit">Edit</a>&nbsp;&nbsp;&nbsp;
-                <a href="" className="post-delete">Delete</a>
-              </div>
-            </div>
-          </li>
-          </ol>
-        </div>
-
+        <Header />
+        <PostList posts={this.state.posts}/>
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class PostList extends Component {
+
+    render() {
+
+        const { posts } = this.props;
+
+        return (
+
+            <div className="list-posts">
+                <ol className="post-list">
+                    {
+                        posts.map(post => (
+                            <Post
+                                id={post.id}
+                                timestamp={post.timestamp}
+                                title={post.title}
+                                body={post.body}
+                                author={post.author}
+                                category={post.category}
+                                voteScore={post.voteScore}
+                            />
+                        ))
+                    }
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default PostList;
